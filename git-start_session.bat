9ea0c5e4ef19fdc7fa71fef804d6d09175b25215
petercl8 <lindstrom.peter@gmail.com>
2025-11-15 17:37:15 -0600

    Quick automatic update

--- git-start_session.bat
+++ git-start_session.bat
@@ -1,9 +1,5 @@
 @echo off
 :: ===== Start Session =====
-
-git remote remove origin 2>nul
-git remote add origin git@github.com:petercl8/PET_Machine_Learning_Datamake.git
-
 :: Fetch latest changes from GitHub
 git fetch origin
 
